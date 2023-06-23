05298e63983aca2b03fedbc1822c7de8ae458a6c
Brenda <brenda.alves@ccc.ufcg.edu.br>
2023-06-23 00:08:32 -0300

    arquivos gerados durante a demo

--- src/endpoints.js
+++ src/endpoints.js
@@ -4,4 +4,9 @@ module.exports = function (app) {
     app.get('/', (req, res) => {
        res.status(200).send('Aplicação base para demo sobre Helm');
     })
+
+    app.get('/hello-world', (req, res) => {
+        res.status(200).send('Hello world!');
+     })
+    
 }
